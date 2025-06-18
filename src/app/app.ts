08662ccecf7ffa172ca0cd495871904945fdd5da
elisha <elishajameel2007@gmail.com>
2025-06-18 18:19:56 +0500

    for loop contextual variables in angular

--- src/app/app.ts
+++ src/app/app.ts
@@ -7,24 +7,6 @@ import { Component, effect, signal } from '@angular/core';
   styleUrl: './app.css',
 })
 export class App {
-  username = signal('Elisha');
-  count = signal(0);
-  displayHeading: boolean = false;
-
-  constructor() {
-    effect(() => {
-      if (this.count() == 2) {
-        this.displayHeading = true;
-        setTimeout(() => {
-          this.displayHeading = false;
-        }, 2000);
-      } else {
-        this.displayHeading = false;
-      }
-    });
-  }
-  toggleValue() {
-    this.displayHeading = !this.displayHeading;
-    this.count.set(this.count() + 1);
-  }
+  users = ['Elisha', 'Sam', 'Bruce', 'Smith', 'Ryan', 'Jameel', 'Noman'];
+  // users = [];
 }
