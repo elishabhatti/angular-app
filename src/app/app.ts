38bba7036619a59058965a680a4526d23ec1cd6e
elisha <elishajameel2007@gmail.com>
2025-06-19 13:48:44 +0500

    ng swtch case in angular

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,20 +1,15 @@
-import { NgFor, NgIf } from '@angular/common';
+import { NgFor, NgIf, NgSwitch, NgSwitchCase, NgSwitchDefault } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  imports: [NgIf, NgFor],
+  imports: [NgIf, NgFor, NgSwitch, NgSwitchCase, NgSwitchDefault],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
 export class App {
-  login = false;
-  block = 0;
-
-  updateBlock() {
-    this.block++;
-    if (this.block == 4) {
-      this.block = 0;
-    }
+  color: string = 'red';
+  changeColor(val:string) {
+    this.color = val
   }
 }
