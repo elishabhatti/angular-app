e53c48739d4abf90f3e67edee69ba0845d866c49
elisha <elishajameel2007@gmail.com>
2025-06-19 13:37:45 +0500

    ngIf directive in angular

--- src/app/app.ts
+++ src/app/app.ts
@@ -8,13 +8,13 @@ import { Component } from '@angular/core';
   styleUrl: './app.css',
 })
 export class App {
-  students = ['Elisha', 'Noman', 'Ryan', 'Bruce', 'Yash', 'Babloo', 'Joel'];
-  studentsData = [
-    { name: 'Elisha', age: 18, email: 'elisha@gmail.com' },
-    { name: 'Noman', age: 21, email: 'noman@gmail.com' },
-    { name: 'Ryan', age: 18, email: 'ryan@gmail.com' },
-    { name: 'Joel', age: 17, email: 'joel@gmail.com' },
-    { name: 'Babloo', age: 16, email: 'babloo@gmail.com' },
-    { name: 'Bruce', age: 54, email: 'bruce@gmail.com' },
-  ];
+  login = false;
+  block = 0;
+
+  updateBlock() {
+    this.block++;
+    if (this.block == 4) {
+      this.block = 0;
+    }
+  }
 }
