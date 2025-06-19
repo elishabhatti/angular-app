1e7ff7c49f112e9c0126f38e73c43ff27336bbab
elisha <elishajameel2007@gmail.com>
2025-06-19 14:17:53 +0500

    created 404 page for un created routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { About } from './about/about';
 import { Login } from './login/login';
 import { Contact } from './contact/contact';
 import { Home } from './home/home';
+import { PageNotFound } from './page-not-found/page-not-found';
 
 export const routes: Routes = [
   { path: '', component: Home },
   { path: 'about', component: About },
   { path: 'login', component: Login },
   { path: 'contact', component: Contact },
+  { path: '**', component: PageNotFound },
 ];
