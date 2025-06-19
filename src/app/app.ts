3e5759ee5e7abf2adfa6999cf5edcd9dced3b4ab
elisha <elishajameel2007@gmail.com>
2025-06-19 13:18:31 +0500

    directive in angular ngif

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,20 +1,16 @@
+import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [NgIf],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
 export class App {
-  bgColor: string = 'red';
-  fontSize: string = '30';
-  headingSizeBig: string = '80px';
-  headingSizeSmall: string = '40px';
+  show:boolean = false;  
 
-  zoom: boolean = false;
-
-  updateHeadingSize() {
-    this.zoom = !this.zoom;
+  hideShowHeading() {
+    this.show = !this.show
   }
 }
