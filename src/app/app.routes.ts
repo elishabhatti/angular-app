70382e1f0f1ef002ca538ddc7b9ddbd373b6e281
elisha <elishajameel2007@gmail.com>
2025-06-19 14:08:20 +0500

    routing with header in angular

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,8 +2,10 @@ import { Routes } from '@angular/router';
 import { About } from './about/about';
 import { Login } from './login/login';
 import { Contact } from './contact/contact';
+import { Home } from './home/home';
 
 export const routes: Routes = [
+  { path: '', component: Home },
   { path: 'about', component: About },
   { path: 'login', component: Login },
   { path: 'contact', component: Contact },
