de82f72beb00eab5cbe0c237ab66f53bd9723034
elisha <elishajameel2007@gmail.com>
2025-06-19 13:28:02 +0500

    directives in angular ngFor array of objects

--- src/app/app.ts
+++ src/app/app.ts
@@ -9,4 +9,12 @@ import { Component } from '@angular/core';
 })
 export class App {
   students = ['Elisha', 'Noman', 'Ryan', 'Bruce', 'Yash', 'Babloo', 'Joel'];
+  studentsData = [
+    { name: 'Elisha', age: 18, email: 'elisha@gmail.com' },
+    { name: 'Noman', age: 21, email: 'noman@gmail.com' },
+    { name: 'Ryan', age: 18, email: 'ryan@gmail.com' },
+    { name: 'Joel', age: 17, email: 'joel@gmail.com' },
+    { name: 'Babloo', age: 16, email: 'babloo@gmail.com' },
+    { name: 'Bruce', age: 54, email: 'bruce@gmail.com' },
+  ];
 }
