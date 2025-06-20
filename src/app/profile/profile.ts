a051ce9242c08c031d103ec2b3ba85deb62c7a28
elisha <elishajameel2007@gmail.com>
2025-06-20 12:17:26 +0500

    pass data with routerLink in angular

--- src/app/profile/profile.ts
+++ src/app/profile/profile.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-profile',
   imports: [],
   templateUrl: './profile.html',
-  styleUrl: './profile.css'
+  styleUrl: './profile.css',
 })
 export class Profile {
-
+  userName: string | null = '';
+  constructor(private route: ActivatedRoute) {}
+  ngOnInit(): void {
+    this.userName = this.route.snapshot.paramMap.get('name');
+    console.log(this.userName);
+  }
 }
