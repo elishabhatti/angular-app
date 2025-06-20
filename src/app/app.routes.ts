3750fbdd1c6796704c8dfce00c1cab4235a565ab
elisha <elishajameel2007@gmail.com>
2025-06-20 12:06:40 +0500

    added profile page navigation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,11 +4,13 @@ import { Login } from './login/login';
 import { Contact } from './contact/contact';
 import { Home } from './home/home';
 import { PageNotFound } from './page-not-found/page-not-found';
+import { Profile } from './profile/profile';
 
 export const routes: Routes = [
   { path: '', component: Home },
   { path: 'about', component: About },
   { path: 'login', component: Login },
   { path: 'contact', component: Contact },
+  { path: 'profile', component: Profile },
   { path: '**', component: PageNotFound },
 ];
