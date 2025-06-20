1afe058d214994d45d90053f9abe9143e065ea07
elisha <elishajameel2007@gmail.com>
2025-06-20 12:27:26 +0500

    pass data with button in angular

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   imports: [RouterLink],
   templateUrl: './home.html',
-  styleUrl: './home.css'
+  styleUrl: './home.css',
 })
 export class Home {
-
+  constructor(private router: Router) {}
+  goToProfilePage(name: string) {
+    this.router.navigate(['profile'], {
+      queryParams: { name },
+    });
+  }
 }
--- src/app/profile/profile.ts
+++ src/app/profile/profile.ts
@@ -11,7 +11,12 @@ export class Profile {
   userName: string | null = '';
   constructor(private route: ActivatedRoute) {}
   ngOnInit(): void {
-    this.userName = this.route.snapshot.paramMap.get('name');
-    console.log(this.userName);
+    // this.userName = this.route.snapshot.paramMap.get('name');
+    // console.log(this.userName);
+
+    this.route.queryParams.subscribe((params) => {
+      this.userName = params['name'];
+      console.log(params['name']);
+    });
   }
 }
