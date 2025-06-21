e2e924cad6e561ac1a882327d9a7b97a8f666be1
elisha <elishajameel2007@gmail.com>
2025-06-21 12:47:11 +0500

    basic reactive forms in angular

--- src/app/contact/contact.ts
+++ src/app/contact/contact.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './contact.html',
-  styleUrl: './contact.css'
+  styleUrl: './contact.css',
 })
 export class Contact {
+  name = new FormControl();
+  password = new FormControl();
 
+  displayValue(e:Event) {
+    e.preventDefault()
+    console.log(this.name.value, this.password.value);
+  }
 }
