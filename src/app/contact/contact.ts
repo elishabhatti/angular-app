8db9275ffbab060b83e3735bc27a675d23b3a7b6
elisha <elishajameel2007@gmail.com>
2025-06-21 13:41:48 +0500

    template driven forms in angular

--- src/app/contact/contact.ts
+++ src/app/contact/contact.ts
@@ -1,47 +1,16 @@
-import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
-import {
-  FormControl,
-  FormGroup,
-  ReactiveFormsModule,
-  Validators,
-} from '@angular/forms';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
-  imports: [ReactiveFormsModule, NgIf],
+  imports: [FormsModule],
   templateUrl: './contact.html',
   styleUrl: './contact.css',
 })
 export class Contact {
-  profileForm = new FormGroup({
-    name: new FormControl('', [Validators.required]),
-    age: new FormControl('', [Validators.required]),
-    email: new FormControl('', [
-      Validators.required,
-      Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,4}$'),
-      Validators.maxLength(50),
-    ]),
-    password: new FormControl('', [
-      Validators.required,
-      Validators.minLength(5),
-    ]),
-  });
-
-  onSubmit() {
-    console.log(this.profileForm.value);
-  }
-
-  get name() {
-    return this.profileForm.get('name');
-  }
-  get age() {
-    return this.profileForm.get('age');
-  }
-  get email() {
-    return this.profileForm.get('email');
-  }
-  get password() {
-    return this.profileForm.get('password');
+  userDetails:any = {};
+  addDetails(val: NgForm) {
+    console.log(val);
+    this.userDetails = val
   }
 }
