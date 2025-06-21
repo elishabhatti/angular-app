bb2d65f2225b625b229f0c63559485e00a5aedd3
elisha <elishajameel2007@gmail.com>
2025-06-21 13:29:45 +0500

    reactive form validation in angular

--- src/app/contact/contact.ts
+++ src/app/contact/contact.ts
@@ -1,29 +1,47 @@
+import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, NgIf],
   templateUrl: './contact.html',
   styleUrl: './contact.css',
 })
 export class Contact {
   profileForm = new FormGroup({
-    name: new FormControl('Elisha'),
-    age: new FormControl('18'),
-    email: new FormControl('elisha@gmail.com'),
-    password: new FormControl('elisha@#$'),
+    name: new FormControl('', [Validators.required]),
+    age: new FormControl('', [Validators.required]),
+    email: new FormControl('', [
+      Validators.required,
+      Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,4}$'),
+      Validators.maxLength(50),
+    ]),
+    password: new FormControl('', [
+      Validators.required,
+      Validators.minLength(5),
+    ]),
   });
 
   onSubmit() {
     console.log(this.profileForm.value);
   }
-  setValues() {
-    this.profileForm.setValue({
-      name: "Guest",
-      age: "18",
-      email: "guest@gmail.com",
-      password : "guest@#$"
-    })
+
+  get name() {
+    return this.profileForm.get('name');
+  }
+  get age() {
+    return this.profileForm.get('age');
+  }
+  get email() {
+    return this.profileForm.get('email');
+  }
+  get password() {
+    return this.profileForm.get('password');
   }
 }
