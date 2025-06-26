14ca876737142eafac86649549de2f8471ea8055
elisha <elishajameel2007@gmail.com>
2025-06-26 16:31:56 +0500

    apply lazy loading in angular

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,57 +1,10 @@
 import { Component } from '@angular/core';
-import { Users } from './services/users';
-import { User } from './interfaces/User';
-import { FormsModule } from '@angular/forms';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [FormsModule],
+  imports: [RouterOutlet, RouterLink],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
-export class App {
-  users: User[] = [];
-  selectedUser: User | undefined;
-
-  constructor(private userService: Users) {}
-
-  getUsers() {
-    this.userService.getUsers().subscribe((data: User[]) => {
-      this.users = data;
-    });
-  }
-  addUser(user: User) {
-    if (!this.selectedUser) {
-      this.userService.saveUsers(user).subscribe((data: User) => {
-        if (data) {
-          this.getUsers();
-        }
-      });
-    } else {
-      const userData = { ...user, id: this.selectedUser.id };
-      this.userService.updateUser(userData).subscribe((data: User) => {
-        if (userData) {
-          this.getUsers();
-        }
-      });
-    }
-  }
-
-  deleteUser(id: string) {
-    this.userService.deleteUser(id).subscribe((data: User) => {
-      if (data) {
-        this.getUsers();
-      }
-    });
-  }
-
-  selectUser(id: string) {
-    this.userService.getSelectedUser(id).subscribe((data: User) => {
-      console.log(data);
-      this.selectedUser = data;
-    });
-  }
-  ngOnInit() {
-    this.getUsers();
-  }
-}
+export class App {}
