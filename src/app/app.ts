d0b716ce463d5a355cc5c7491784547a0deac54d
elisha <elishajameel2007@gmail.com>
2025-07-02 19:30:51 +0500

    added styling in form

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { Form } from "./form/form";
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [Form],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
